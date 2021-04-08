d1cd11b2cec693cbf9cf132c6cce07805a70de51
almostSouji <timoqueezle@gmail.com>
2021-04-08 12:12:04 +0200

    feat(Commands): remove prefixless commands

--- src/events/commandHandler/noCommand.ts
+++ src/events/commandHandler/noCommand.ts
@@ -1,9 +1,6 @@
 import EventHandler from '../../handlers/EventHandler';
 import { Event } from '../../structures/Event';
-import { Message, TextChannel, Guild } from 'discord.js';
-import { Args, Token, ParserOutput } from 'lexure';
-import { ExecutionContext } from '../../structures/Command';
-import { PREFIXES } from '../../util/constants';
+import { Message, TextChannel } from 'discord.js';
 import TagCommand from '../../commands/tag';
 
 export default class extends Event {
@@ -14,45 +11,11 @@ export default class extends Event {
 		});
 	}
 
-	private allowPrefixless(channelID: string, guild?: Guild | null) {
-		if (!guild) return true;
-		const allowed = this.handler.client.guildSettings.get(guild.id)?.prefixless_allowed_channels ?? [];
-		return allowed.includes(channelID);
-	}
-
 	public async execute(message: Message): Promise<boolean> {
 		const { client } = this.handler;
 
 		if (message.channel instanceof TextChannel && !message.channel.permissionsFor(message.client.user!)?.has(['SEND_MESSAGES'])) {
 			return false;
-		}
-
-		if (this.allowPrefixless(message.channel.id, message.guild)) {
-			for (const command of client.commands.commands.values()) {
-				if (!command.regExp) continue;
-				const match = command.regExp.exec(message.content);
-				if (!match) continue;
-
-				const [, ...args] = match;
-				const tokens: Token[] = args.filter(v => v).map(s => ({ raw: s, trailing: '', value: s }));
-				const out: ParserOutput = {
-					ordered: tokens,
-					flags: new Set(),
-					options: new Map()
-				};
-
-				try {
-					if (message.channel instanceof TextChannel && !message.channel.permissionsFor(message.client.user!)?.has(command?.clientPermissions)) {
-						return false;
-					}
-
-					await command.execute(message, new Args(out), '', ExecutionContext['REGEXP']);
-				} catch (error) {
-					message.answer(`${PREFIXES.FAIL}${error.message}`);
-				}
-
-				break;
-			}
 		}
 
 		const match = client.commands.prefixRegExp(message.guild).exec(message.content)?.[0] ?? null;
--- src/util/constants.ts
+++ src/util/constants.ts
@@ -24,27 +24,6 @@ export const LOAD = {
 	WAIT_DURATION: 3000
 };
 
-export const GITHUB = {
-	BASE: 'https://api.github.com/graphql',
-	EMOJI: {
-		DELETE: '🗑'
-	},
-	REACTION_TIMEOUT: 5000,
-	GITHUB_BASE_URL: 'https://api.github.com/graphql',
-	GITHUB_ICON_PR_OPEN: 'https://cdn.discordapp.com/emojis/759112631508008960.png',
-	GITHUB_ICON_PR_CLOSED: 'https://cdn.discordapp.com/emojis/759110235574370305.png',
-	GITHUB_ICON_PR_MERGED: 'https://cdn.discordapp.com/emojis/759113184509558884.png',
-	GITHUB_ICON_PR_DRAFT: 'https://cdn.discordapp.com/emojis/759111607711563797.png',
-	GITHUB_ICON_ISSUE_OPEN: 'https://cdn.discordapp.com/emojis/759114372491902976.png',
-	GITHUB_ICON_ISSUE_CLOSED: 'https://cdn.discordapp.com/emojis/759114382101184532.png',
-	GITHUB_ICON_COMMIT: 'https://cdn.discordapp.com/emojis/759112647383056384.png',
-	GITHUB_COLOR_OPEN: 4491332,
-	GITHUB_COLOR_CLOSED: 14166056,
-	GITHUB_COLOR_MERGED: 7559322,
-	GITHUB_COLOR_DRAFT: 13421772,
-	GITHUB_COLOR_COMMIT: 1668818
-};
-
 export const COLORS = {
 	SUCCESS: '#03b581',
 	FAIL: '#d04949',
@@ -98,79 +77,6 @@ export const MESSAGES = {
 				USAGE: (usage: string) => `${PREFIXES.FAIL}Command usage: \`${usage}\`.`,
 				RESOLVE: (query: string, type: string) => `${PREFIXES.FAIL}I can not resolve \`${query}\` to a \`${type}\`.`
 			}
-		},
-		GITHUB: {
-			ALIAS: {
-				ERRORS: {
-					NO_GUILD: `${PREFIXES.FAIL}Please use this command in the server you want to edit settings for`,
-					NO_SUB: (validCommands: string) => `${PREFIXES.FAIL}Missing sub-command, valid arguments are ${validCommands}.`,
-					INVALID_SUB: (command: string, validCommands: string) => `${PREFIXES.FAIL}Invalid sub-command \`${command}\`. Valid arguments are: ${validCommands}`,
-					PERMISSIONS: `${PREFIXES.FAIL}You need the permission "ADMINISTRATOR" or "MANAGE_GUILD" in order to modify GitHub aliases.`
-				},
-				ALIAS_FORMAT: '`alias:owner/repository`',
-				ADD: {
-					NO_ARGS: 'Please provide valid repository aliases to add. Valid format: `alias:owner/repository`. Also make sure the desired alias does not already exist.',
-					NO_ADD: 'No new repository aliases were added.',
-					TITLE: 'The following repository aliases have been enabled on this server:'
-				},
-				REMOVE: {
-					NO_ARGS: 'Please provide valid repository aliases to remove. Valid format: `alias:owner/repository`.',
-					NO_CURRENT: 'This server does not have any repository aliases enabled at this time.',
-					NO_REMOVE: 'No repository aliases have been removed.',
-					TITLE: 'The following repository aliases have been disabled on this server:'
-				},
-				LIST: {
-					NO_CURRENT: 'This server does not have any repository aliases at this time.',
-					TITLE: 'The following repository aliases are enabled on this server:'
-				}
-			},
-			COMMIT: {
-				ERRORS: {
-					NOT_FOUND: (expression: string, owner: string, repository: string) => `Could not find a commit based on ${expression} on the repository ${owner}/${repository}.`
-				},
-				FILES: (count: number) => `${count} file${count > 1 ? 's' : ''} changed.`
-			},
-			ERRORS: {
-				FETCH: `${PREFIXES.FAIL}Error while trying to fetch GitHub API information.`,
-				NO_RESULT: `${PREFIXES.FAIL}Could not find any results.`,
-				ARGS_MISSING: `${PREFIXES.FAIL}Missing arguments, correct usage: \n• \`github <owner> <repository> <issue>\` \n• \`github <alias> <issue>\`\n• \`github alias <add|remove> <...alias>\`\n• \`github alias list\``,
-				NO_TOKEN: 'Could not access GitHub token to use.',
-				INVALID_OWNER: `${PREFIXES.FAIL}Please provide a valid GitHub repository owner.`,
-				INVALID_REPOSITORY: `${PREFIXES.FAIL}Please provide a valid GitHub repository.`,
-				INVALID_ISSUE: `${PREFIXES.FAIL}Please provide a valid issue number.`
-			},
-			ISSUE_PR: {
-				ACTION: {
-					CLOSE: 'closed',
-					DRAFT: 'draft opened',
-					MERGED_BY_IN: (user: string, commit: string) => `merged by ${user} in ${commit}`,
-					MERGED_BY: (user: string) => `merged by ${user}`,
-					MERGED_IN: (commit: string) => `merged in ${commit}`,
-					MERGE: 'merged',
-					OPEN: 'opened'
-				},
-				COMMENTS: (count: number) => `${count} comment${count > 1 ? 's' : ''}`,
-				ERRORS: {
-					NOT_FOUND: (issue: number, owner: string, repository: string) => `${PREFIXES.FAIL}Could not find issue ${issue} on the repository ${owner}/${repository}.`
-				},
-				HEADING: {
-					INSTALL: 'Install with',
-					REVIEWS: {
-						APPROVED: 'Reviews (status: approved)',
-						REVIEW_REQUIRED: 'Reviews (status: review required)',
-						CHANGES_REQUESTED: `Reviews (status: changes requested)`
-					}
-				},
-				REVIEW_STATE: {
-					APPROVED: 'approved',
-					REVIEW_REQUIRED: 'review required',
-					CHANGES_REQUESTED: 'changes requested',
-					COMMENTED: 'commented',
-					DISMISSED: 'dismissed',
-					PENDING: 'pending'
-				}
-			},
-			UNKNOWN: 'unknown'
 		},
 		HELP: {
 			AVAILABLE: 'Your available commands are',
