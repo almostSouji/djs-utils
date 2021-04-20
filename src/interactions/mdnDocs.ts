ddd06ab77194c698783f2f4da2ac3c56e437a211
almostSouji <timoqueezle@gmail.com>
2021-04-20 13:28:49 +0200

    feat(interactions): mdn without ducksearch, remove ducksearch util

--- src/interactions/mdnDocs.ts
+++ src/interactions/mdnDocs.ts
@@ -1,35 +1,38 @@
+import fetch from 'node-fetch';
 import { Response } from 'polka';
 import { logger } from '../util/logger';
-import TurndownService from 'turndown';
-import { duckSearch, DuckSearchSite } from '../util/duckSearch';
 import { prepareErrorResponse, prepareResponse } from '../util/respond';
+import { encode } from 'querystring';
 
 const API_BASE = 'https://developer.mozilla.org';
 const MDN_ICON = '<:mdn:818272565419573308>';
-const td = new TurndownService();
+
+const cache = new Map<string, any>();
 
 export async function mdnSearch(res: Response, query: string, target?: string): Promise<Response> {
 	try {
-		const searchResult = await duckSearch(query, DuckSearchSite.mdn);
-		if (!searchResult) {
-			prepareErrorResponse(res, 'No search result found. Maybe try later or try a different query!');
+		const qString = `${API_BASE}/api/v1/search?${encode({ q: query })}`;
+		let hit = cache.get(qString);
+		if (!hit) {
+			const result = await fetch(qString).then(r => r.json());
+			hit = result.documents?.[0];
+			cache.set(qString, hit);
+		}
+
+		if (!hit) {
+			prepareErrorResponse(res, `No search result found for qurey \`${query}\``);
 			return res;
 		}
 
-		if (!searchResult.data) {
-			prepareResponse(res, `${target ? `*Documentation suggestion for <@${target}>:*\n` : ''}Unable to parse received data, but maybe this is useful: <${searchResult.searchURL}>`, false, target ? [target] : []);
-			return res;
-		}
+		const url = `${API_BASE}${hit.mdn_url}`;
 
-		const url = `${API_BASE}${searchResult.data.doc.mdn_url}`;
-
-		const parts = [`${MDN_ICON} \ __[**${searchResult.data.doc.title}**](<${url}>)__`];
-
-		const intro = td.turndown(searchResult.data.doc.body[0].value.content ?? 'no intro').split('\n\n')[0];
 		const linkReplaceRegex = /\[(.+?)\]\((.+?)\)/g;
 		const boldCodeBlockRegex = /`\*\*(.*)\*\*`/g;
+		const intro = hit.summary.replace(/\n/g, ' ')
+			.replace(linkReplaceRegex, `[$1](${API_BASE}<$2>)`)
+			.replace(boldCodeBlockRegex, '**`$1`**');
 
-		parts.push(intro.replace(linkReplaceRegex, `[$1](${API_BASE}<$2>)`).replace(boldCodeBlockRegex, '**`$1`**'));
+		const parts = [`${MDN_ICON} \ __[**${hit.title}**](<${url}>)__`, intro];
 
 		prepareResponse(res, `${target ? `*Documentation suggestion for <@${target}>:*\n` : ''}${parts.join('\n')}`, false, target ? [target] : []);
 		return res;
