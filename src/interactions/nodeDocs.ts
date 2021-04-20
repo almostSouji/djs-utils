a66ee5b257be2a42519310ab0bb21f6c143c85a6
almostSouji <timoqueezle@gmail.com>
2021-04-20 12:18:43 +0200

    feat(interactions): node without ducksearch

--- src/interactions/nodeDocs.ts
+++ src/interactions/nodeDocs.ts
@@ -1,71 +1,74 @@
 import { Response } from 'polka';
+import fetch from 'node-fetch';
 import { logger } from '../util/logger';
-import { duckSearch, DuckSearchSite } from '../util/duckSearch';
 import TurndownService from 'turndown';
-import * as cheerio from 'cheerio';
 import { prepareErrorResponse, prepareResponse } from '../util/respond';
 
 const API_BASE = 'https://nodejs.org';
 const NODE_ICON = '<:node_js:818292297644245103>';
 const td = new TurndownService({ codeBlockStyle: 'fenced' });
 
-type QueryType = 'method' | 'class' | 'event' | 'classMethod';
+type QueryType = 'method' | 'class' | 'event' | 'classMethod' | 'module';
 
-function findRec(o: any, name: string, type: QueryType): any {
+function findRec(o: any, name: string, type: QueryType, module?: string): any {
 	name = name.toLowerCase();
-	if (o?.name?.toLowerCase() === name.toLowerCase() && o?.type === type) return o;
+	if (!module) module = o?.type === 'module' ? o?.name.toLowerCase() : undefined;
+	if (o?.name?.toLowerCase() === name.toLowerCase() && o?.type === type) {
+		o.module = module;
+		return o;
+	}
 	for (const prop of Object.keys(o)) {
 		if (Array.isArray(o[prop])) {
 			for (const entry of o[prop]) {
-				const res = findRec(entry, name, type);
-				if (res) return res;
+				const res = findRec(entry, name, type, module);
+				if (res) {
+					o.module = module;
+					return res;
+				}
 			}
 		}
 	}
 }
 
+let data: any = null;
+
+function anchor(text: string, module: string): string {
+	const method = text
+		.toLowerCase()
+		.replace(/ |`|\[|\]|\)/g, '')
+		.replace(/\.|\(|,|:/g, '_');
+	return `${module}_${method}`;
+}
+
 export async function nodeSearch(res: Response, query: string, target?: string): Promise<Response> {
 	try {
-		const page = await duckSearch(query, DuckSearchSite.node);
-		if (!page) {
-			prepareErrorResponse(res, 'No search result found. Maybe try later or try a different query!');
-			return res;
-		}
-
-		if (!page.data) {
-			prepareResponse(res, `${target ? `*Documentation suggestion for <@${target}>:*\n` : ''}Unable to parse received data, but maybe this is useful: <${page.searchURL}>`, false, target ? [target] : []);
-			return res;
+		if (!data) {
+			data = await fetch(`${API_BASE}/dist/latest/docs/api/all.json`).then(r => r.json());
 		}
 
 		const queryParts = query.split(/#|\.|\s/);
 		const altQuery = queryParts[queryParts.length - 1];
 
 		const result =
-			findRec(page.data, query, 'class') ??
-			findRec(page.data, query, 'classMethod') ??
-			findRec(page.data, query, 'method') ??
-			findRec(page.data, query, 'event') ??
-			findRec(page.data, altQuery, 'class') ??
-			findRec(page.data, altQuery, 'method') ??
-			findRec(page.data, altQuery, 'event') ??
-			findRec(page.data, altQuery, 'classMethod');
+			findRec(data, query, 'class') ??
+			findRec(data, query, 'classMethod') ??
+			findRec(data, query, 'method') ??
+			findRec(data, query, 'event') ??
+			findRec(data, altQuery, 'class') ??
+			findRec(data, altQuery, 'method') ??
+			findRec(data, altQuery, 'event') ??
+			findRec(data, altQuery, 'classMethod') ??
+			findRec(data, query, 'module') ??
+			findRec(data, altQuery, 'module');
 
 		if (!result) {
-			if (page.searchURL.includes(query)) {
-				prepareResponse(res, `${target ? `*Documentation suggestion for <@${target}>:*\n` : ''}Unable to parse received data, but maybe this is useful: <${page.searchURL}>`, false, target ? [target] : []);
-				return res;
-			}
-			prepareErrorResponse(res, 'No result found in input data.');
+			prepareErrorResponse(res, `No result found for query \`${query}\`.`);
 			return res;
 		}
 
-		const heading = result.type === 'method' ? result.textRaw.replaceAll('`', '') : result.name;
-
-		const $ = cheerio.load(page.htmlPage);
-		const codeElement = $(`code:contains("${heading}")`);
-		const elementId = codeElement.parent().find('span a').attr('id');
-
-		const parts = [`${NODE_ICON} \ __[**${result.textRaw}**](<${page.searchURL}#${elementId}>)__`];
+		const moduleURL = `${API_BASE}/api/${result.module as string}`;
+		const fullURL = `${moduleURL}.html${result.type === 'module' ? '' : `#${anchor(result.textRaw, result.module)}`}`;
+		const parts = [`${NODE_ICON} \ __[**${result.textRaw as string}**](<${fullURL}>)__`];
 
 		const intro = td.turndown(result.desc ?? 'no intro').split('\n\n')[0];
 		const linkReplaceRegex = /\[(.+?)\]\((.+?)\)/g;
