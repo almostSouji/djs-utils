20783109b2438d4ace32f6d4fa08550f0f003de8
almostSouji <timoqueezle@gmail.com>
2021-06-06 23:46:32 +0200

    fix: more inclusive interaction handling

--- src/server.ts
+++ src/server.ts
@@ -36,43 +36,48 @@ export async function start(vars: Runvariables) {
 	polka()
 		.use(jsonParser(), verify)
 		.post('/interactions', async (req, res) => {
-			const message = req.body;
-			logger.log('explore', `${chalk.blue(inspect(message, { depth: null }))}`);
-			if (message.type === 2) {
-				const { data: { name, id, options }, member } = message;
+			try {
+				const message = req.body;
+				logger.log('explore', `${chalk.blue(inspect(message, { depth: null }))}`);
+				if (message.type === 2) {
+					const { data: { name, id, options }, member } = message;
 
-				logger.log('explore', `${name} (${id}) by ${chalk.blue(`${member.user.username}#${member.user.discriminator}`)}`);
+					logger.log('explore', `${name} (${id}) by ${chalk.blue(`${member.user.username}#${member.user.discriminator}`)}`);
 
-				if (options?.length) {
-					const args = Object.fromEntries(options.map(({ name, value }: { name: string; value: any }) => [name, value]));
+					if (options?.length) {
+						const args = Object.fromEntries(options.map(({ name, value }: { name: string; value: any }) => [name, value]));
 
-					if (name === 'docs') {
-						return (await djsDocs(res, args.source ?? 'stable', args.query, args.target)).end();
+						if (name === 'docs') {
+							return (await djsDocs(res, args.source ?? 'stable', args.query, args.target)).end();
+						}
+
+						if (name === 'guide') {
+							return (await djsGuide(res, args.query, args.results, args.target)).end();
+						}
+
+						if (name === 'mdn') {
+							return (await mdnSearch(res, args.query, args.target)).end();
+						}
+
+						if (name === 'node') {
+							return (await nodeSearch(res, args.query, args.target)).end();
+						}
 					}
 
-					if (name === 'guide') {
-						return (await djsGuide(res, args.query, args.results, args.target)).end();
+					if (name === 'invite') {
+						prepareResponse(res, `Add the discord.js interaction to your server: [(click here)](<https://discord.com/api/oauth2/authorize?client_id=${vars.clientID}&scope=applications.commands>)`, true);
+						return res.end();
 					}
 
-					if (name === 'mdn') {
-						return (await mdnSearch(res, args.query, args.target)).end();
-					}
-
-					if (name === 'node') {
-						return (await nodeSearch(res, args.query, args.target)).end();
-					}
+					logger.warn(`Unknown interaction received: ${chalk.yellow(name)} guild: ${chalk.yellow(message.guild_id)}`);
 				}
-
-				if (name === 'invite') {
-					prepareResponse(res, `Add the discord.js interaction to your server: [(click here)](<https://discord.com/api/oauth2/authorize?client_id=${vars.clientID}&scope=applications.commands>)`, true);
-					return res.end();
-				}
-
-				logger.warn(`Unknown interaction received: ${chalk.yellow(name)} guild: ${chalk.yellow(message.guild_id)}`);
+				logger.warn(`Received interaction of type ${chalk.yellow(message.type)}`);
+				prepareResponse(res, `\`🐞${message.type}\` This shouldn't be there...`, true);
+				res.end();
+			} catch (error) {
+				logger.error(error);
+				prepareResponse(res, `\`🍵418\` Looks like something wrent wrong here, please try again later!`, true);
 			}
-			logger.warn(`Received interaction of type ${chalk.yellow(message.type)}`);
-			prepareResponse(res, `\`🐞${message.type}\` This shouldn't be there...`, true);
-			res.end();
 		})
 		.listen(vars.port);
 	logger.log('ok', `Listening for interactions on port ${chalk.green(vars.port)}`);
